1a9063e7eeeae623010700d7298a33ceea4b1f24
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-02-15 20:15:39 +0100

    add expose hook to handle events of changing windown, minimizing etc

--- src/8_hooks.c
+++ src/8_hooks.c
@@ -27,9 +27,16 @@ int	sl_key_press(int keycode, t_game *game)
 	return (0);
 }
 
+int	sl_render_game_again(t_game *sl)
+{
+	if (sl->mlx.win != NULL)
+		mlx_put_image_to_window(sl->mlx.mlx, sl->mlx.win, sl->img.img, 0, 0);
+	return (0);
+}
+
 void	sl_mlx_hooks(t_game *sl)
 {
-	//mlx_expose_hook(sl->mlx.win, &sl_render_game, sl);
+	mlx_expose_hook(sl->mlx.win, &sl_render_game_again, sl);
 	//mlx_mouse_hook();
 	mlx_hook(sl->mlx.win, DestroyNotify, NoEventMask, &close_game, sl);
 	mlx_hook(sl->mlx.win, KeyPress, KeyPressMask, &sl_key_press, sl);
