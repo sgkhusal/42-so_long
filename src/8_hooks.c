41658cc7dd956392c737c929a1c86912bb927f03
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-02-15 20:19:25 +0100

    Add error message in the render_game_again function

--- src/8_hooks.c
+++ src/8_hooks.c
@@ -31,14 +31,15 @@ int	sl_render_game_again(t_game *sl)
 {
 	if (sl->mlx.win != NULL)
 		mlx_put_image_to_window(sl->mlx.mlx, sl->mlx.win, sl->img.img, 0, 0);
+	else
+		sl_error("Not possible to render game. Mlx window is set as NULL.", sl);
 	return (0);
 }
 
 void	sl_mlx_hooks(t_game *sl)
 {
 	mlx_expose_hook(sl->mlx.win, &sl_render_game_again, sl);
-	//mlx_mouse_hook();
 	mlx_hook(sl->mlx.win, DestroyNotify, NoEventMask, &close_game, sl);
 	mlx_hook(sl->mlx.win, KeyPress, KeyPressMask, &sl_key_press, sl);
-	//mlx_loop_hook(sl->mlx.mlx, &sl_render_game, sl);
+	mlx_loop_hook(sl->mlx.mlx, &sl_render_game_again, sl);
 }
